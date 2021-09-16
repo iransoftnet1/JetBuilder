e52996ce5e23b061863a2a96b85aca8c67cabe7c
hassan <iransoftnet.ir@gmail.com>
2021-09-16 14:00:39 +0430

    feat: folder zip

--- src/local/index.php
+++ src/local/index.php
@@ -1,18 +1,29 @@
 <?php
+require_once 'zip.php';
 header( 'Content-type: text/html; charset=utf-8' );
-echo 'Begin ...<br />';
-$output = shell_exec("cd /d F:\landing-bornafit-front && npm run build");
+
+echo '----BEGIN...<br />';
+
+//build
+echo '<p>build...</p>';
+/*$output = shell_exec("cd /d F:\landing-bornafit-front && npm run build");
 var_dump($output);
 flush();
-ob_flush();
-sleep(2);
-echo 'upload ...';
+ob_flush();*/
+echo '<p>** build success **</p>';
+
+
+//convert file to zip
+echo '<p>zip archive...</p>';
+$zip = new convertZip();
+$zip->convert('F:\landing-bornafit-front\dist');
+
+
 flush();
 ob_flush();
-sleep(2);
-echo 'success ...';
+echo '<p>** zip archive **</p>';
 
-echo 'End ...<br />';
+echo '<p>----END</p>';
 
 
 
---
+++ src/local/zip.php
@@ -0,0 +1,40 @@
+<?php
+
+class convertZip
+{
+    public function __construct()
+    {
+    }
+
+    public function convert($path)
+    {
+        // Get real path for our folder
+        $rootPath = realpath($path);
+
+        // Initialize archive object
+        $zip = new ZipArchive();
+        $zip->open('file.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        // Create recursive directory iterator
+        /** @var SplFileInfo[] $files */
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($rootPath),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $name => $file) {
+            // Skip directories (they would be added automatically)
+            if (!$file->isDir()) {
+                // Get real and relative path for current file
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+                // Add current file to archive
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        // Zip archive will be created only after closing object
+        $zip->close();
+    }
+}
